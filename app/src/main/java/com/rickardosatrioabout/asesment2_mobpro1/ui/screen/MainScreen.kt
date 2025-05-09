08e7d98f46fbdaa281c1680902d9b8caf3b3cd44
Rickardo Satrio <rickardosatrio@gmail.com>
2025-05-09 15:30:33 +0700

    Membuat tampilan list

--- app/src/main/java/com/rickardosatrioabout/asesment2_mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/rickardosatrioabout/asesment2_mobpro1/ui/screen/MainScreen.kt
@@ -1,8 +1,15 @@
 package com.rickardosatrioabout.asesment2_mobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +18,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rickardosatrioabout.asesment2_mobpro1.R
+import com.rickardosatrioabout.asesment2_mobpro1.model.ukm
 import com.rickardosatrioabout.asesment2_mobpro1.ui.theme.Asesment2_mobpro1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,11 +50,45 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn (
+        modifier.fillMaxSize()
+    ){
+        items(data){
+            UkmListItem(ukm = it)
+            HorizontalDivider()
+        }
+    }
 }
+
+@Composable
+fun UkmListItem(ukm: ukm) {
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = ukm.namaukm,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = ukm.namaketua)
+        Text(
+            text = ukm.kontak,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = ukm.deskripsi,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
 
 
 
